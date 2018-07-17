4cacbf9b19f46f531edc60ffa3e5e6d7c0f77baa
wbigger <wbigger@gmail.com>
2018-07-17 23:03:34 +0200

    Remove Calendar dependency from Main Class

--- examples/AirShowManager/src/airshow/AirShowManager.java
+++ examples/AirShowManager/src/airshow/AirShowManager.java
@@ -5,7 +5,6 @@
  */
 package airshow;
 
-import calendarmanager.CalendarEvent;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
@@ -20,18 +19,18 @@ public class AirShowManager {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        CalendarEvent[] events= new CalendarEvent[3];
+        AirShow[] events= new AirShow[3];
         
         events[0] = new AirShow("Festa dei piloti",LocalDate.of(2018, 6, 29));
-        ((AirShow)events[0]).setCategory(AirShowCategory.FUN);
+        events[0].setCategory(AirShowCategory.FUN);
         
         events[1] = new AirShow("Arona Air Show",LocalDate.of(2018, 6, 30));
-        ((AirShow)events[0]).setCategory(AirShowCategory.BUSINESS);
+        events[0].setCategory(AirShowCategory.BUSINESS);
         
         events[2] = new AirShow("Palermo Air Show",LocalDate.of(2018, 7, 7));
-        ((AirShow)events[0]).setCategory(AirShowCategory.BUSINESS);        
+        events[0].setCategory(AirShowCategory.BUSINESS);        
         
-        for (CalendarEvent event : events) {
+        for (AirShow event : events) {
             System.out.format("Event: %s, from %s to %s, category %s%n", event.getTitle(),event.getStartDate().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)),event.getEndDate().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)),event.getCategory()==null?"none":event.getCategory());
         }
     }
